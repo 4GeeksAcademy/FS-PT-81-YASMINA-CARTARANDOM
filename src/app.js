5a5fcdc2126f3b663c6ff9ba7cfafb232505e318
Yabel90 <yasminabelhaj@hotmail.com>
2024-10-21 20:45:55 +0000

    my random card in javascript

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  console.log("evento onload");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const valores = [
+    "A",
+    "j",
+    "k",
+    "Q",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const aleatorioPalos = palos[Math.floor(Math.random() * palos.length)];
+  const aleatorioValores = valores[Math.floor(Math.random() * valores.length)]; //write your code hereç
+  document.querySelector(".card-paloarriba").innerText = aleatorioPalos;
+  document.querySelector(".card-valor").innerText = aleatorioValores;
+  document.querySelector(".card-paloabajo").innerText = aleatorioPalos;
+  if (aleatorioPalos == "♦" || aleatorioPalos == "♥") {
+    document.querySelector(".card-paloarriba").classList.add("red");
+    document.querySelector(".card-paloabajo").classList.add("red");
+    document.querySelector(".card-valor").classList.add("red");
+  }
 };
